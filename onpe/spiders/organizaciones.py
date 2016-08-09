1e781f5e730b81d3c1566eb37b9b95dcfc0f401c
Jason Martinez <jason.p.martinez.v@gmail.com>
2016-08-09 15:18:45 -0500

    organizaciones

---
+++ onpe/spiders/organizaciones.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from onpe.items import AporteItem
+import re
+import pymongo
+
+client = pymongo.MongoClient()
+db = client.onpe
+collection = db.personasJuridicas
+
+TIPO_ORGANIZACION = ["PARTIDO POLITICO", "ALIANZA ELECTORAL", "MOVIMIENTO REGIONAL", "ORGANIZACION POLITICA PROVINCIAL", "ORGANIZACION POLITICA DISTRITAL"]
+
+class FinanciamientoSpider(scrapy.Spider):
+    name = "financiamiento"
+    allowed_domains = []
+
+
+    def start_requests(self):
+        page_url = 'https://www.onpe.gob.pe/loadSelect/'
+
+        for tipo in range(1, 5):
+
+            params = {
+                'vType': 'aportes',
+                'vValu': str(tipo),
+            }
+
+            meta = {
+                'tipoOrganizacion': TIPO_ORGANIZACION[tipo],
+            }
+
+            yield scrapy.FormRequest(url=page_url, formdata=params, meta=meta,
+                callback=self.load_select_request)
+
+    def load_select_request(self, response):
+
+        page_url = 'https://www.onpe.gob.pe/loadDetail/'
+
+        options = response.xpath("//option")
+
+        for option in options:
+            obj = {
+                'tipoDoc': "RUC",
+                'numDoc':  str(option.xpath('@value').extract_first()),
+                'razonSocial': option.xpath('./text()').extract_first(),
+                'tipoOrg': "Organización Política",
+                'tipoOrgPol': response.meta['tipoOrganizacion'],
+            }
+            collection.save(obj)
